7d8f75ac8bf6f6da10db1d91652608a332ddbd44
10553527 <10553527@mydbs.ie>
2022-02-18 21:47:53 +0000

    Add project files.

---
+++ B8IT150_SQ_RegEx/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace B8IT150_SQ_RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cont = true;
+
+            Console.Write("File Path: ");
+            string file = Console.ReadLine();
+
+            if (file == "")
+            {
+                Environment.Exit(0);
+            }
+
+            do
+            {
+                Console.Write("Regular Expression: ");
+                string regEx = Console.ReadLine();
+
+                if (regEx == "")
+                {
+                    Environment.Exit(0);
+                }
+
+                Regex engine = new Regex(regEx);
+
+                try
+                {
+                    using (StreamReader rdr = new StreamReader(file))
+                    {
+                        string line;
+
+                        while ((line = rdr.ReadLine()) != null)
+                        {
+                            Match match = engine.Match(line);
+
+                            if (match.Success)
+                            {
+                                Console.WriteLine($"Match at {match.Index}");
+                                Console.WriteLine($"Line: {line}");
+                            }
+                        }
+                    }
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("The file does not exist at the path provided.");
+                }
+
+                Console.WriteLine("Press any key to continue or 'N' to exit.");
+                string something = Console.ReadLine();
+
+                if (something == "N")
+                {
+                    cont = false;
+                }
+            }
+            while (cont == true);
+        }
+    }
+}
